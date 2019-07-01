08d6a87242f486c5815b06cde566a902b9cd9804
Jeremiah <jeremiah.towe@gmail.com>
2019-07-01 18:22:30 -0400

    Base API to come from env and apiKey->accessToken rename

--- src/Helpers/spApi.php
+++ src/Helpers/spApi.php
@@ -6,25 +6,24 @@ namespace App\Helpers;
 class spApi
 {
     private $_curl;
-    private $_apiKey;
+    private $_accessToken;
     private $_response;
     private $_httpStatusCode;
 
-    public function __construct($apiKey)
+    public function __construct($accessToken)
     {
-        $this->_apiKey = $apiKey;
+        $this->_accessToken = $accessToken;
         $this->_curl = curl_init();
         curl_setopt($this->_curl, CURLOPT_RETURNTRANSFER, true);
-
     }
 
     public function get(String $path)
     {
         curl_setopt($this->_curl, CURLOPT_HTTPHEADER, array(
-            "Authorization: Bearer " . $this->_apiKey,
+            "Authorization: Bearer " . $this->_accessToken,
             "User-Agent: JeremiahApp/0.0.1"
         ));
-        curl_setopt($this->_curl, CURLOPT_URL, $_ENV('BASE_SP_API_URL') . $path);
+        curl_setopt($this->_curl, CURLOPT_URL, $_ENV['BASE_SP_API_URL'] . $path);
         $this->_response = curl_exec($this->_curl);
         $this->_httpStatusCode = curl_getinfo($this->_curl, CURLINFO_HTTP_CODE);
 
@@ -39,11 +38,11 @@ class spApi
     public function post(String $path, $item)
     {
         curl_setopt($this->_curl, CURLOPT_HTTPHEADER, array(
-            "Authorization: Bearer " . $this->_apiKey,
+            "Authorization: Bearer " . $this->_accessToken,
             "User-Agent: JeremiahApp/0.0.1",
             "Content-Type: application/vnd.shootproof+json"
         ));
-        curl_setopt($this->_curl, CURLOPT_URL, $_ENV('BASE_SP_API_URL') . $path);
+        curl_setopt($this->_curl, CURLOPT_URL, $_ENV['BASE_SP_API_URL'] . $path);
         curl_setopt($this->_curl, CURLOPT_POST, count($item));
         curl_setopt($this->_curl, CURLOPT_POSTFIELDS, json_encode($item));
         $this->_response = curl_exec($this->_curl);
