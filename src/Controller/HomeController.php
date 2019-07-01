b071ef8d7cbe5d3acb9246bfde5b71756741febd
Jeremiah <jeremiah.towe@gmail.com>
2019-06-30 21:52:29 -0400

    spStudio DI into home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,25 @@
 
 namespace App\Controller;
 
+use App\Models\ShootProof\spStudio;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $studio;
+
+    public function __construct(spStudio $studio)
+    {
+        $this->studio = $studio;
+    }
+
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
