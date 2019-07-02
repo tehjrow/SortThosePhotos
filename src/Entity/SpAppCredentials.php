bb709b31a2ad66e2e345cc7338c2eca1f1d29dec
Jeremiah <jeremiah.towe@gmail.com>
2019-07-02 19:37:18 -0400

    Added SpAppCredentials entity

---
+++ src/Entity/SpAppCredentials.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SpAppCredentialsRepository")
+ */
+class SpAppCredentials
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $responseType;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $clientId;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $redirectUri;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $scope;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $state;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getResponseType(): ?string
+    {
+        return $this->responseType;
+    }
+
+    public function setResponseType(string $responseType): self
+    {
+        $this->responseType = $responseType;
+
+        return $this;
+    }
+
+    public function getClientId(): ?string
+    {
+        return $this->clientId;
+    }
+
+    public function setClientId(string $clientId): self
+    {
+        $this->clientId = $clientId;
+
+        return $this;
+    }
+
+    public function getRedirectUri(): ?string
+    {
+        return $this->redirectUri;
+    }
+
+    public function setRedirectUri(string $redirectUri): self
+    {
+        $this->redirectUri = $redirectUri;
+
+        return $this;
+    }
+
+    public function getScope(): ?string
+    {
+        return $this->scope;
+    }
+
+    public function setScope(string $scope): self
+    {
+        $this->scope = $scope;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
---
+++ src/Repository/SpAppCredentialsRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\SpAppCredentials;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method SpAppCredentials|null find($id, $lockMode = null, $lockVersion = null)
+ * @method SpAppCredentials|null findOneBy(array $criteria, array $orderBy = null)
+ * @method SpAppCredentials[]    findAll()
+ * @method SpAppCredentials[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SpAppCredentialsRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, SpAppCredentials::class);
+    }
+
+    // /**
+    //  * @return SpAppCredentials[] Returns an array of SpAppCredentials objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?SpAppCredentials
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
