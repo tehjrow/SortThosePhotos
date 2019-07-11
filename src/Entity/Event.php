e1bb42ecbda4628cc77db2d3d10a7bb678665f9a
Jeremiah <jeremiah.towe@gmail.com>
2019-07-10 22:13:22 -0400

    Create event entity, repo, and migration

---
+++ src/Entity/Event.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EventRepository")
+ */
+class Event
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $spId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSpId(): ?int
+    {
+        return $this->spId;
+    }
+
+    public function setSpId(?int $spId): self
+    {
+        $this->spId = $spId;
+
+        return $this;
+    }
+}
