95e5dc23665023e5681f86c4ec6918bc31e936e7
Jeremiah <jeremiah.towe@gmail.com>
2019-07-11 10:42:02 -0400

    Add event form submit handling

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
 use App\Form\EventFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
@@ -13,17 +16,35 @@ class EventController extends AbstractController
      */
     public function index()
     {
+        $events = $this->getDoctrine()
+            ->getRepository(Event::class)
+            ->findAll();
+
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
+            'events' => $events,
         ]);
     }
 
     /**
      * @Route("/event/new", name="event_new")
      */
-    public function new()
+    public function new(EntityManagerInterface $em, Request $request)
     {
+        // https://symfonycasts.com/screencast/symfony-forms/form-submit#play
         $form = $this->createForm(EventFormType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $data = $form->getData();
+            $event = new Event();
+            $event->setName($data['name']);
+
+            $em->persist($event);
+            $em->flush();
+
+            return $this->redirectToRoute('event');
+        }
 
         return $this->render('event/new.html.twig',[
             'eventForm' => $form->createView()
