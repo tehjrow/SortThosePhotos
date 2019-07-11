1a9eec67aab63e5437526a39fad3a1e169484538
Jeremiah <jeremiah.towe@gmail.com>
2019-07-10 22:12:29 -0400

    Create EventController and index Twig

---
+++ src/Controller/EventController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+    /**
+     * @Route("/event", name="event")
+     */
+    public function index()
+    {
+        return $this->render('event/index.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+
+    /**
+     * @Route("/event/new", name="event_new")
+     */
+    public function new()
+    {
+        return $this->render('event/new.html.twig');
+    }
+}
