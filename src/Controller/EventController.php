ed8810f6dcb9fb1be8bcfdd60e7ad60e8767df2d
Jeremiah <jeremiah.towe@gmail.com>
2019-07-21 16:42:13 -0400

    Add upload action for csv file

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -12,9 +12,11 @@ use App\Entity\Event;
 use App\Form\EventFormType;
 use App\Models\ViewModels\EventViewModel;
 use App\Models\ViewModels\ServiceDetails;
+use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -77,7 +79,7 @@ class EventController extends AbstractController
      *
      * Return single even and view
      */
-    public function event(EntityManagerInterface $em, $id)
+    public function event($id)
     {
         $event = $this->getDoctrine()
             ->getRepository(Event::class)
@@ -98,8 +100,34 @@ class EventController extends AbstractController
     /**
      * @Route("/event/{id}/csv", name="event_csv", requirements={"id"="\d+"})
      */
-    public function eventUploadCsv($id)
+    public function eventUploadCsv(EntityManagerInterface $em, Request $request, FileUploader $fileUploader, $id)
     {
-        return $this->render('csv/csv.html.twig');
+        $event = $this->getDoctrine()
+            ->getRepository(Event::class)
+            ->find($id);
+
+        $form = $this->createForm(EventFormType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            /** @var UploadedFile $csvFile */
+            $csvFile = $form['csv']->getData();
+            if ($csvFile)
+            {
+                $csvFilename = $fileUploader->upload($csvFile, 'csv');
+                $event->setCsvFilename($csvFilename);
+                $event->setHasUploadedCsv(true);
+
+                $em->persist($event);
+                $em->flush();
+
+                return $this->redirectToRoute('event',['id' => $event->getId()]);
+            }
+        }
+
+        return $this->render('csv/csv.html.twig',[
+            'eventForm' => $form->createView()
+        ]);
     }
 }
