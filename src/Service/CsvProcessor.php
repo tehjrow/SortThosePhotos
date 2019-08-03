3ddabd91dc84260a9bfcf2bc23dbb45fe29682e5
Jeremiah <jeremiah.towe@gmail.com>
2019-08-03 13:42:29 -0400

    Make csvprocessor check for albums already in db and update if found

--- src/Service/CsvProcessor.php
+++ src/Service/CsvProcessor.php
@@ -21,22 +21,15 @@ class CsvProcessor
         $this->albumRepository = $albumRepository;
     }
 
-    public function processCsv(int $userId, string $eventId, string $fileName)
+    public function processCsv(int $userId, int $eventId, string $fileName)
     {
         $csvPath = $this->uploadBaseDirectory . '/csv/' . $userId . '/' . $fileName;
         $albumArray = $this->csvFileToArray($csvPath);
-        $this->storeAlbumsInDatabase($userId, $albumArray, $eventId);
-
-
+        $this->storeAlbumsInDatabase($userId, $eventId, $albumArray);
     }
 
-    public function storeAlbumsInDatabase($userId, $albumArray, $eventId)
+    private function storeAlbumsInDatabase(int $userId, int $eventId, array $albumArray)
     {
-        $albumsInEvent = $this->albumRepository->findBy([
-            'eventId' => $eventId
-        ]);
-        dd($albumsInEvent, $albumArray);
-        // TODO make sure album isn't already in db before adding
         foreach ($albumArray as $albumInArray)
         {
             $album = new Album();
@@ -44,12 +37,41 @@ class CsvProcessor
             $album->setEventId($eventId);
             $album->setName($albumInArray["Album Name"]);
             $album->setPassword($albumInArray["Password"]);
-            $this->em->persist($album);
+
+            $albumFromDatabase = $this->albumExistsInDatabase($album);
+
+            if ($albumFromDatabase)
+            {
+                $albumFromDatabase->setName($albumInArray["Album Name"]);
+                $albumFromDatabase->setPassword($albumInArray["Password"]);
+                $this->em->persist($albumFromDatabase);
+            }
+            else
+            {
+                $this->em->persist($album);
+            }
         }
         $this->em->flush();
     }
 
-    public function csvFileToArray($csvPath)
+    private function albumExistsInDatabase(Album $album)
+    {
+        $albumsInEvent = $this->albumRepository->findBy([
+            'eventId' => $album->getEventId(),
+            'userId' => $album->getUserId()
+        ]);
+
+        foreach ($albumsInEvent as $albumInEvent)
+        {
+            if ($albumInEvent->getName() == $album->getName())
+            {
+                return $albumInEvent;
+            }
+        }
+        return null;
+    }
+
+    private function csvFileToArray($csvPath): array
     {
         $rows = array_map('str_getcsv', file($csvPath));
 
