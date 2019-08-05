288803795db34f0b901765196afad40fc0f09a77
Jeremiah <jeremiah.towe@gmail.com>
2019-08-05 17:19:11 -0400

    Pull albums and generate QR codes which are sent to the QR index

--- src/Controller/QRController.php
+++ src/Controller/QRController.php
@@ -2,18 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Album;
+use Skies\QRcodeBundle\Generator\Generator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QRController extends AbstractController
 {
     /**
-     * @Route("/q/r", name="q_r")
+     * @Route("/event/{id}/qr", name="qr", requirements={"id"="\d+"})
      */
-    public function index()
+    public function index($id)
     {
+        $albums = $this->getDoctrine()
+            ->getRepository(Album::class)
+            ->findBy([
+                'eventId' => $id
+            ]);
+
+        $barcodes = '';
+
+        foreach ($albums as $album)
+        {
+            $options = array(
+                'code'   => strval($album->getId()),
+                'type'   => 'qrcode',
+                'format' => 'svg',
+                'width'  => 10,
+                'height' => 10,
+                'color'  => 'green',
+            );
+            $generator = new Generator();
+            $barcode = $generator->generate($options);
+            $barcodes .= $barcode;
+        }
+
         return $this->render('qr/index.html.twig', [
-            'controller_name' => 'QRController',
+            'barcodes' => $barcodes,
         ]);
     }
 }
